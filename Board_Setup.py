4d811fe649f3b68b813b9a7b6d1edaa43a52d1f7
turnerdj95 <60992092+turnerdj95@users.noreply.github.com>
2020-03-06 15:53:21 -0500

    Update Board_Setup.py

--- Board_Setup.py
+++ Board_Setup.py
@@ -1,27 +1,50 @@
 import numpy as np
 
-class Board:
-    def __init__(self, rows, columns):
-        self.rows = int(rows)
-        self.columns = int(columns)
-        self.board = np.zeros((self.rows, self.columns), dtype=int)
-        self.player1 = 1
-        self.player2 = -1
+class Board():
+	def __init__(self,rows,columns):
+		self.rows=int(rows)
+		self.columns=int(columns)
+		self.board = np.zeros((self.cors,self.columns),dtype='int8')
+		self.p1 = 1
+		self.p2 = -1
 
-    def player_one(self, col_select):
-        col = self.board[:, col_select]
-        counter = self.rows - 1
-        while col[0] != 0:
-            col = self.board[:, int(input("That column is full.  Please select a different column: "))]
-        while col[counter] != 0:
-            counter -= 1
-        self.board[counter, col_select] = self.player1
+	def player_one(self,col_select):
+		while col_select > self.columns-1:
+			col_select = int(input("Invalid column.  Please select a number between 0-{}: ".format(self.rows-1)))
+		col = self.board[:,col_select]
+		counter = self.rows-1
+		while col[counter] != 0
+			counter -= 1
+		self.board[counter,col_select] = self.p1
 
-    def player_two(self, col_select):
-        col = self.board[:, col_select]
-        counter = self.rows - 1
-        while col[0] != 0:
-            col = int(input("That column is full.  Please select a different column: "))
-        while col[counter] != 0:
-            counter -= 1
-        self.board[counter, col_select] = self.player2
+	def player_two(self,col_select):
+		while col_select > self.columns-1:
+			col_select = int(input("Invalid column.  Please select a number between 0-{}: ".format(self.rows-1)))
+		col = self.board[:,col_select]
+		counter = self.rows-1
+		while col[counter] != 0
+			counter -= 1
+		self.board[counter,col_select] = self.p2
+
+
+def check_x(rows,columns):
+	board = Board(rows,columns)
+	while 0 in board.board:
+	try:
+		move1 = np.random.randint(0,board.columns)
+		while 0 not in board.board[:,move1]:
+			move1 = np.random.randint(0,board.columns)
+			if 0 not in board.board:
+				break
+		board.player_one(move1)
+		print(board.board,'\n')
+
+		move2 = np.random.randint(0,board.columns)
+		while 0 not in board.board[:,move2]:
+			move2 = np.random.randint(0,board.columns)
+			if 0 not in board.board:
+				break
+		board.player_one(move2)
+		print(board.board,'\n')
+	except IndexError:
+		continue
